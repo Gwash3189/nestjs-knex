d7bd4cfac9b972938a51b5711d32cdd989d5b7d7
Adam Beck <adamb3189@gmail.com>
2021-04-07 09:10:23 +1000

    temp: check in dist as part of src

---
+++ dist/knex.interfaces.d.ts
@@ -0,0 +1,16 @@
+import { ModuleMetadata, Type } from "@nestjs/common/interfaces";
+import * as knex from 'knex';
+export declare type Knex = typeof knex;
+export declare type Connection = Knex;
+export interface KnexModuleOptions {
+    config: knex.Knex.Config;
+}
+export interface KnexModuleOptionsFactory {
+    createKnexModuleOptions(): Promise<KnexModuleOptions> | KnexModuleOptions;
+}
+export interface KnexModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+    inject?: any[];
+    useClass?: Type<KnexModuleOptionsFactory>;
+    useExisting?: Type<KnexModuleOptionsFactory>;
+    useFactory?: (...args: any[]) => Promise<KnexModuleOptions> | KnexModuleOptions;
+}
